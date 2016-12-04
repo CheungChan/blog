6f3777eda91cab8216d8742fb02dcb6fa5ba14e4
cheungchan <1377699408@qq.com>
2016-12-04 18:34:41 +0800

    git推送脚本更新

--- git/autopush.sh
+++ git/autopush.sh
@@ -1,20 +1,21 @@
 #!bin/sh
-
-#if the script self name not in ./.info/exclude,just write append to the file
+set -o nounset
+set -o errexit
+# if the script self name not in ./.info/exclude,just write append to the file
 excludeAddr="./.info/exclude"
 excludeInfo=$(cat $excludeAddr|grep $0)
 if [ -z "$excludeInfo" ]; then
     echo $0>>$excludeAddr
 fi
-#get the origin branch
-#e.g
-#"master"
+# get the origin branch
+# e.g
+# "master"
 oribr=$(git branch | sed -n '/A/s/\s//gp')
 #
 #
-#get the current branch
-#e.g
-#"dev" "dev~1 dev~0 " "dev~2 dev~1 dev~0 "
+# get the current branch
+# e.g
+# "dev" "dev~1 dev~0 " "dev~2 dev~1 dev~0 "
 curbr=$(git branch | sed -n '\* /s///p')
 s=""
 num=1
@@ -32,8 +33,20 @@ else
     exit 1
 fi
 ##
-##begin git command
+# #begin git command
 ##
-echo "**********初始化脚本成功**********"
-git checkout $oribr && echo "**********切换到${oribr}成功**********" && git cherry-pick $s && echo "**********优选${num}个提交成功**********" && git pull --rebase --progress "origin" && echo "**********衍合拉取成功**********" && git push --recurse-submodules=check --progress "origin" refs/heads/$oribr:refs/heads/$oribr && echo "**********推送成功**********" && git checkout $curbr && echo "**********切换到${curbr}分支成功**********" && git rebase $oribr && echo "**********衍合成功**********" && echo "**********脚本执行成功O(∩_∩)O哈哈~**********"
+echo "(1/7)初始化脚本成功"
+git checkout $oribr
+echo "(2/7)切换到${oribr}成功"
+git cherry-pick $s
+echo "(3/7)优选${num}个提交成功"
+git pull --rebase --progress "origin"
+echo "(4/7)衍合拉取成功"
+git push --recurse-submodules=check --progress "origin" refs/heads/$oribr:refs/heads/$oribr
+echo "(5/7)推送成功"
+git checkout $curbr 
+echo "(6/7)切换到${curbr}分支成功"
+git rebase $oribr
+echo "(7/7)衍合成功"
+echo "脚本执行成功"
 
